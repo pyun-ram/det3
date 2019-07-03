78d207eb259645bebf5adba860cd4b2cecd50116
pyun <pyun@connect.ust.hk>
2019-07-03 22:03:47 +0800

    update data augmentation

--- methods/voxelnet/create_dataset.py
+++ methods/voxelnet/create_dataset.py
@@ -29,30 +29,33 @@ def main():
     dataset = args.dataset
     data_dir = args.data_dir
     num_of_train = args.num_of_train
-    os.makedirs(os.path.join(data_dir, "dev", "fast_load"), exist_ok=False)
-    os.makedirs(os.path.join(data_dir, "val", "fast_load"), exist_ok=False)
     cfg.bool_fast_loader = False # It is important, otherwise the fast load data will not be updated.
     if dataset == "KITTI":
         from det3.methods.voxelnet.kittidata import KittiDatasetVoxelNet
         for mode in ["dev", "val"]:
-            print(mode)
-            dataloader = KittiDatasetVoxelNet(data_dir=data_dir, train_val_flag=mode, cfg=cfg)
-            for _, data in enumerate(dataloader):
-                print("{:06d}".format(data[0]))
-                pickle.dump(data, open("{}/{}/{}/{:06d}.pkl".format(data_dir, mode, "fast_load", data[0]), "wb"))
-        mode = "train"
-        for i in range(num_of_train):
-            print(mode+str(i))
-            target_dir = os.path.join(data_dir, mode, "fast_load_{}".format(i))
             try:
+                print(mode)
+                target_dir = os.path.join(data_dir, mode, "fast_load")
                 os.makedirs(target_dir, exist_ok=False)
+                dataloader = KittiDatasetVoxelNet(data_dir=data_dir, train_val_flag=mode, cfg=cfg)
+                for _, data in enumerate(dataloader):
+                    print("{:06d}".format(data[0]))
+                    pickle.dump(data, open("{}/{:06d}.pkl".format(target_dir, data[0]), "wb"))
             except FileExistsError:
                 print("FileExistsError RAISED: {} has already been created!".format(target_dir))
-                continue
-            dataloader = KittiDatasetVoxelNet(data_dir=data_dir, train_val_flag=mode, cfg=cfg)
-            for _, data in enumerate(dataloader):
-                print("{:06d}".format(data[0]))
-                pickle.dump(data, open("{}/{:06d}.pkl".format(target_dir, data[0]), "wb"))
+
+        mode = "train"
+        for i in range(num_of_train):
+            try:
+                print(mode+str(i))
+                target_dir = os.path.join(data_dir, mode, "fast_load_{}".format(i))
+                os.makedirs(target_dir, exist_ok=False)
+                dataloader = KittiDatasetVoxelNet(data_dir=data_dir, train_val_flag=mode, cfg=cfg)
+                for _, data in enumerate(dataloader):
+                    print("{:06d}".format(data[0]))
+                    pickle.dump(data, open("{}/{:06d}.pkl".format(target_dir, data[0]), "wb"))
+            except FileExistsError:
+                print("FileExistsError RAISED: {} has already been created!".format(target_dir))
 
     elif dataset == "CARLA":
         from det3.methods.voxelnet.carladata import CarlaDatasetVoxelNet
