fd05e42efc6b6f6a16990be2847b3bec6bb7c28b
pyun <pyun@connect.ust.hk>
2019-07-17 16:52:24 +0800

    first commit of torch_utils

---
+++ utils/torch_utils.py
@@ -0,0 +1,10 @@
+'''
+File Created: Wednesday, 17th July 2019 4:49:47 pm
+Author: Peng YUN (pyun@ust.hk)
+Copyright 2018 - 2019 RAM-Lab, RAM-Lab
+'''
+import sys
+
+class GradientLogger:
+    def __init__(self):
+        self.data = dict()
